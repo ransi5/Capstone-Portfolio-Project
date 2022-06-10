f060f4b66969951b7b8f82a54a577dcd2cfdaa57
Randeep Gill <ransi.gill@gmail.com>
2022-06-10 13:25:45 -0700

    content update

--- js/headJS.js
+++ js/headJS.js
@@ -1,23 +1,38 @@
 const project = [
   {
-    projName: '"Pearson Test of English Education" Project',
-    image: 'images/project-0.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-1.git',
-    ghpages: 'https://github.com/ransi5/pte-preparation.git',
+    projName: 'Full Stack App with React and Express REST API',
+    image: 'images/project-1.jpg',
+    GitHub: 'https://github.com/ransi5/Treehouse-FSJD-Project-10.git',
     details: `<h3 class="highlight">Key Features</h3>
-    <p>I initiated this project in 2016 with absolutely no knowledge of coding.</p>
-    <p>I quickly self-taught HTML, CSS, JavaScript, JQuery, Bootstrap, PHP, SQL and
-    finished the Project as per schedule working 8 months over a span of 1 year.</p>
-    <p>I designed, developed the code and content Single-handedly.</p>
-    <p>I designed and developed a module which helped give student key performance metrics at
-    macro and micro Language skills level to focus on in dynamic graphs. The
-    students could choose target score to generate PTE skills they need to
-    work on to meet their goals.</p>
-    <p>At the time and may be even today my PTE mock tests most closely simulated
-    the actual PTE test.</p>
-    <p>The SERP focused design and content development helped the site be No. 1
-    for many important keywords in key markets within a year.</p>
-    <p class='highlight'>Get in touch for login and sql table to run this website on localhost</p>`
+    <p>This app's UI is built with React and the REST API is powered by Express to manage a SQL database.</p>
+    <p>The App allows user to view, edit, delete and create courses on a school database.</p>
+    <p>The app allows user to register and signin.</p>
+    <p>What a user can view and actions one can perform is controlled by user authentication.</p>`
+  },
+  {
+    projName: 'React Gallery App',
+    image: 'images/project-2.jpg',
+    GitHub: 'https://github.com/ransi5/Treehouse-FSJD-Project-7.git',
+    details: `<h3 class="highlight">Key Features</h3>
+    <p>The React powered API allows uses flickr api to render images requested by user.</p>
+    <p>The app uses pagination feature and allows user to choose the number of images per page.</p>`
+  },
+  {
+    projName: 'SQL Library Manager',
+    image: 'images/project-3.jpg',
+    GitHub: 'https://github.com/ransi5/Treehouse-FSJD-Project-8.git',
+    details: `<h3 class="highlight">Key Features</h3>
+    <p>This Express and pug powered app allows user to view, update, delete and create books in a SQL library.</p>
+    <p>The app provides search and a beautiful pagination feature which allows user to control number of books displayed per page.</p>`
+  },
+  {
+    projName: 'Static Node.js and Express Portfolio Site',
+    image: 'images/project-4.jpg',
+    GitHub: 'https://github.com/ransi5/Treehouse-FSJD-Project-6.git',
+    details: `<h3 class="highlight">Key Features</h3>
+    <p>Used Node.js, Express and Pug template to create a portfolio website to display projects</p>
+    <p>Provided user a modal form to set up cookies and display customized messages depending
+    on user choice.</p>`
   },
   {
     projName: 'TreeHouse Capstone Portfolio Project',
@@ -35,182 +50,32 @@ const project = [
     <p>Coded imperfections and corrections to make the site appear alive and aware.</p>`
   },
   {
-    projName: 'TreeHouse Web App Dashboard Project',
-    image: 'images/project-7.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-7.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-7/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Great start, you have included the viewport meta tag, used min-width for
-    a mobile-first approach that responds very well to mobile, tablet and desktop
-    viewport sizes, which makes your project nicely responsive!"</p>
-    <p>"You've also used CSS Grid to layout your main elements."</p>
-    <p>"Very well done, you have included your SVG as inline. Great job on styling
-    your SVG icons. You have managed to style them with the same colours as the mockups
-     and expertly use flexbox for their layout."</p>
-    <p>"Excellent use of JavaScript to populate member name, email and join date to each
-    New Member and a activity and time for each member in the Recent Activity.Awesome,
-    they are nicely spaced with the use of flexbox, matching all mockups."</p>
-    <p>"Your navigation functions beautifully due to you adding your ID attributes to
-    the relevant sections. Great job on the smooth transition to each section.Your
-    navigation layouts match all mockups perfectly!"</p>
-    <p>"Awesome, you have added an event listener for functionality to your alert
-    banner allowing a user to close it."</p>
-    <p>"Fabulous use of animation for the hover state on your notification bell. Great
-    job on the marker for notifying the user of new alerts."</p>
-    <p>"Great job on your drop-down display of notification messages."</p>
-    <p>"Great job implementing charts.org for your Traffic, Daily and Mobile charts.
-    Your charts are nicely styled and match the mockups layout-wise."</p>
-    <p>"Excellent work on your js scripting for the functionality of your traffic
-    navigation. Very well done on updating the charts with each click of your nav
-    links."</p>
-    <p>"Great, you've formatted the form excellently for your messaging widget. You
-    have an input element, textarea and submit button."</p>
-    <p>"Wow, excellent customised JS for your error alerts as well as the sent message
-    alert for your form!"</p>
-    <p>"Fantastic job on your original autocomplete JS for your input field."</p>
-    <p>"Very nice! You put some extra work into the settings widget! It matches the
-    mockups and I can update my preferences and then they're saved when I refresh the
-    page. Plus the settings are reset when I click on the cancel button. You've done
-    great work here combining Javascript with local storage settings to get this
-    functioning beautifully."</p>
-    <p>"Wow! You nailed it! You've done a lot of great work putting this project
-    together and it shows! This is a very complex project with a lot of different
-    pieces and you've done a great job of bringing together all of your awesome HTML, CSS
-    and Javascript skills for a project that looks amazing! Nothing can stop you now!
-    Onward to project."</p>`
+    projName: '"Pearson Test of English Education" Project',
+    image: 'images/project-0.jpg',
+    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-1.git',
+    details: `<h3 class="highlight">Key Features</h3>
+    <p>I initiated this project in 2016 with absolutely no knowledge of coding.</p>
+    <p>I quickly self-taught HTML, CSS, JavaScript, JQuery, Bootstrap, PHP, SQL and
+    finished the Project as per schedule working 8 months over a span of 1 year.</p>
+    <p>I designed, developed the code and content Single-handedly.</p>
+    <p>I designed and developed a module which helped give student key performance metrics at
+    macro and micro Language skills level to focus on in dynamic graphs. The
+    students could choose target score to generate PTE skills they need to
+    work on to meet their goals.</p>
+    <p>At the time and may be even today my PTE mock tests most closely simulated
+    the actual PTE test.</p>
+    <p>The SERP focused design and content development helped the site be No. 1
+    for many important keywords in key markets within a year.</p>
+    <p class='highlight'>Get in touch for login and sql table to run this website on localhost</p>`
   },
   {
     projName: 'TreeHouse An Interactive Photo Gallery Project',
     image: 'images/project-5.jpg',
     GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-5.git',
     ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-5/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Great job using css grid to make the gallery responsive! At 1024px window
-    width the gallery has 4 rows and the layout responses smoothly to a change of
-    window size! Cool!"</p>
-    <p>"Great job! You went the extra mile and created your own JavaScript for the search
-    box functionality and it all works really well!"</p>
-    <p>"Great job with this project! Love the fact you've created everything from
-    scratch! You set up your grid to match the mockup beautifully and created your
-    own code for search functionality. These additional features help to provide a
-    user-friendly project that is easy for your user to view and navigate! Well
-    done! "</p>`
-  },
-  {
-    projName: 'TreeHouse Personal Profile Page Project',
-    image: 'images/project-1.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-1.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-1/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Great, you updated the social media links! You updated the Home navigation
-    link to point to index.html instead of #. You correctly linked to and built a
-    2nd page. You then went above and beyond creating an awesome 3rd page!"</p>
-    <p>"Your profile image is under 400kb and your background image is under the 1MB
-    requirement! Sweet, you updated the profile images alt attribute text, and left
-    the "profile-image" class in place! You also added new styles/effects to the
-    profile image and multiple new images throughout the project!"</p>
-    <p>"Great job changing the placeholder text and then going a step beyond and
-    adding in some additional text to your project. Your profile project is looking
-    great! Plus you've changed the tag names to make your HTML more semantic. Very
-    top notch work!"</p>
-    <p>"Great job! Congratulations on successfully completing your first
-    project. You've done a great job here showcasing your newly acquired skills and
-    following all of the instructions to get your project across the finish line.
-    Plus you challenged yourself to take the extra steps and earn yourself the
-    awesome Exceeds Expectations grade! Way to go!"</p>`
-  },
-  {
-    projName: 'TreeHouse Mobile Responsive Layout Project',
-    image: 'images/project-2.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-2.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-2/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Nice job on adding more details to your design. Your profile page looks
-    great. It is responsive and adapts to mobile, tablet and desktop screen sizes.
-    You have added a new section for skills and it looks very nice."</p>
-    <p>"Great! You have used <nav> tag for the navigation bar. and added a skill
-    section. All your links work as expected!"</p>
-    <p>"Awesome! You nailed it! You have done a great job with this project, it
-    looks So good. Keep up the good work. I wish you the best of luck and have
-    fun with the next unit."</p>`
-  },
-  {
-    projName: 'TreeHouse Online Registration Form Project',
-    image: 'images/project-3.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-3.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-3/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3>Reviewer comments</h3>
-    <p>"Great work adding in some extra detail! I see you've added some placeholder
-    text to your input fields, and you've added the "required" attribute to add HTML
-    validation to the required input fields! These extra touches really help your
-    project stand out!"</p>
-    <p>"Your project matches both mockups and is looking amazing! It's
-    great to see you have spent some time adding more details to your design! Taking
-    the time and effort to go above and beyond can really make a difference on
-    honing your skills as a future developer and your project is looking great!"</p>
-    <p>"Really amazing job on this project! You've demonstrated a good
-    understanding of creating forms and using flexbox to make them responsive to
-    screen sizes. And you've added some cool personal styling touches that really
-    make your project look pretty cool. Well done on all of this project! Now onward
-    to Project 4! "</p>`
-  },
-  {
-    projName: 'TreeHouse Web Style Guide Project',
-    image: 'images/project-4.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-4.git',
-    ghpages: ' https://ransi5.github.io/Treefouse-FEWD-Project-4/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Perfect job setting up your components folder, grouping styles together, and
-    using the power of Sass to add in parent and nested selectors!"</p>
-    <p>"Awesome work building out your style guide to match up with the index.html
-    file provided in the resources folder. And well done using the lighten()
-    function to jazz up your buttons!"</p>
-    <p>"Great job building out this project and demonstrating your awesome Sass
-    skills. Well done! :smiley: You've really done some nice work here of taking
-    the extra steps and implementing the Sass lighten() function to earn yourself
-    an exceeds grade for this project."</p>`
-  },
-  {
-    projName: 'TreeHouse Game Show App Project',
-    image: 'images/project-6.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-6.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-6/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Awesome work handling your phrase display and adding a cool transition property
-    here!"</p>
-    <p>"Great work handling your game over screen and adding a button to reset the game.
-    Bravo!"</p>
-    <p>"Wow! Such a great job on this project! You've done an
-    excellent job on bringing all the pieces of this challenging project together
-    for a great project that shows off your Javascript skills. And you even
-    challenged yourself to go above and beyond to get the Exceeds grade! Well
-    done!"</p>`
-  },
-  {
-    projName: 'TreeHouse API generated Employee Direcory',
-    image: 'images/project-8.jpg',
-    GitHub: 'https://github.com/ransi5/Treehouse-FEWD-Project-8.git',
-    ghpages: 'https://ransi5.github.io/Treehouse-FEWD-Project-8/',
-    details: `<p>Achieved <span class="highlight">"Exceed Expectations"</span>
-    grade for this Project</p><h3 class="highlight">Reviewer comments</h3>
-    <p>"Awesome, your app is pulling information for random users from the API!"</p>
-    <p>"Your directory is looking great! I love that you used Sass!"</p>
-    <p>"Sweet, you parsed and displayed all of the required user information into the
-    directory! Then you went above and beyond and built the search functionality to
-    filter users by their name! This is looking like an exceeds so far! "</p>
-    <p>"You have also parsed and displayed all of the required user information into
-    the modal and added the ability to close it! You then again went above and beyond
-    and created a navigation system for the modal!"</p>
-    <p>"You nailed it! :tada: Great job on this project! You've done some amazing work
-    here interacting with the API and also showing off your Javascript skills to build
-    in extra search functionality for your users! Top notch! "</p>`
+    details: `<h3 class="highlight">Key Features</h3>
+    <p>The page displays a gallery of images and allows user to search and view photos in a lighthouse.</p>
+    <p>Developed custom code for search and lighthouse functionalities from scratch</p>`
   },
 ];
 
@@ -243,10 +108,12 @@ function createPortfolio(project) {
     para1.innerHTML = `GitHub: <a href="${item.GitHub}" target="_blank">${item.GitHub}</a>`;
     dv1.appendChild(para1);
 
-    let para2 = document.createElement('p');
-    para2.className = 'gpage'
-    para2.innerHTML = `gh-pages: <a href="${item.ghpages}" target="_blank">${item.ghpages}</a>`;
-    dv1.appendChild(para2);
+    if (item.ghpages) {
+      let para2 = document.createElement('p');
+      para2.className = 'gpage'
+      para2.innerHTML = `gh-pages: <a href="${item.ghpages}" target="_blank">${item.ghpages}</a>`;
+      dv1.appendChild(para2);
+    }
 
     let dv2 = document.createElement('div');
     dv2.className = 'hide';
